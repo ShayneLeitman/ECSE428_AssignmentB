9bdb68f1c9dd31cd8f866470fdc9aba0028a81c9
Shayne Leitman <sleitman17@gmail.com>
2019-03-10 22:17:43 -0400

    Improvements to code

--- src/test/java/stepDefinitions/emailStepDefs.java
+++ src/test/java/stepDefinitions/emailStepDefs.java
@@ -11,7 +11,10 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
 
 public class emailStepDefs {
 
@@ -22,36 +25,68 @@ public class emailStepDefs {
 //    When the user adds an image file attachment
 //    Then the email displays that the image file is attached
 
-	@Given("^the user is creating an email$")
+	
+	//Will run before all tests
+	//Opens up to 
+@Before
+public void setup() {
+	System.setProperty("webdriver.chrome.driver", "C:\\Users\\Robert\\chromedriver_win32\\chromedriver.exe");
+	driver = new ChromeDriver();
+	driver.get("https://accounts.google.com");
+	driver.findElement(By.id("identifierId")).sendKeys("ecse428assb@gmail.com");
+	driver.findElement(By.id("identifierNext")).click();
+	WebDriverWait wait = new WebDriverWait(driver, 10);
+	wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
+	driver.findElement(By.name("password")).sendKeys("testing123!");
+	driver.findElement(By.id("passwordNext")).click();
+}
+	
+/*@After
+public void returnState() {
+	
+	
+	
+}*/
+	
+	
+	@Given("^the user is logged into Gmail$")
 	public void the_user_is_creating_an_email() {
-		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Robert\\chromedriver_win32\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://accounts.google.com");
-		driver.findElement(By.id("identifierId")).sendKeys("ecse428assb@gmail.com");
-		driver.findElement(By.id("identifierNext")).click();
 		WebDriverWait wait = new WebDriverWait(driver, 10);
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("password")));
-		driver.findElement(By.name("password")).sendKeys("testing123!");
-		driver.findElement(By.id("passwordNext")).click();
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("gb")));
-		//Go to Gmail inbox now
 		driver.get("https://mail.google.com/mail/#inbox");
-		//compose new message
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\":3s\"]/div/div")));
-		driver.findElement(By.xpath("//*[@id=\":3s\"]/div/div")).click();
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("to")));
-		driver.findElement(By.name("to")).sendKeys("testing@gmail.com");
-		driver.findElement(By.name("subjectbox")).sendKeys("This is the subject");
-		//driver.findElement(By.id(":a4")).sendKeys("This is the body of the email");
-		WebElement tempPic = driver.findElement(By.name("Filedata"));
-		//driver.findElement(By.id(":af")).click();
-		tempPic.sendKeys("C:\\Users\\Robert\\Pictures\\Camera Roll\\sendtoJack.PNG");
-		//Either of the following can work for the compose button, lower one is better though
-		//*[@id=\":41\"]/div/div
-		//*[@id=":3s"]/div/div
+	}
+		
+	@And("^that the user selects the compose button$")	
+	public void that_the_user_selects_the_compose_buton() {	
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\":41\"]/div/div")));
+		driver.findElement(By.xpath("//*[@id=\":41\"]/div/div")).click();
 	}
 	
+	@When("^the user adds the \"([^\"]*)\" of the recipient$")
+	public void the_user_adds_an_email_recipient(String email) {
+		WebDriverWait wait = new WebDriverWait(driver, 10);
+		wait.until(ExpectedConditions.elementToBeClickable(By.name("to")));
+		driver.findElement(By.name("to")).sendKeys(email + Keys.ENTER);
+	}
 	
+	@And("^adds a message title$")
+	public void add_a_message_title() {
+		driver.findElement(By.name("subjectbox")).sendKeys("This is the title");	
+	}
+	
+	@And("^attaches an \"([^\"]*)\" to the email$")
+	public void attaches_an_image_to_the_email(String imagePath) {
+		WebElement tempPic = driver.findElement(By.name("Filedata"));
+		tempPic.sendKeys("C:\\Users\\Robert\\Pictures\\Camera Roll\\" + imagePath);
+		
+		
+	}
+	
+	/*
+	WebElement tempPic = driver.findElement(By.name("Filedata"));
+	tempPic.sendKeys("C:\\Users\\Robert\\Pictures\\Camera Roll\\sendtoJack.PNG");
+	*/
 	
 /*
 		driver.get("https://accounts.google.com");
--- target/test-classes/Features/email.feature
+++ target/test-classes/Features/email.feature
@@ -2,7 +2,50 @@ Feature: Image attachments in an Email
 
 	# I think this is how to comment in feature files?
 
-  Scenario: User attaches an image file to an outgoing email
-    Given the user is creating an email
-    When the user adds an image file attachment
-    Then the email displays that the image file is attached
+# Normal Flow
+  Scenario Outline: User attaches an image file to an outgoing email to a recipient
+  
+    Given the user is logged into Gmail
+    And that the user selects the compose button
+    When the user adds the "<email>" of the recipient
+    And adds a message title
+    And attaches an "<imagefile>" to the email
+    And the user hits the send button
+    Then the email with the image will be succesfully sent to the correct recipient
+    
+   	Examples:
+    	| email 					| imagefile |
+    	| sleitman17@gmail.com| img1.jpg 	|
+    	| bobby@gmail.com 		| img2.jpg 	|
+    	|	steve@gmail.com 		| img3.jpg 	|
+    	| chris@gmail.com 		| img4.jpg 	|
+    	| nathan@gmail.com 		| img5.jpg 	|
+    
+    
+# Alternate Flow
+	Scenario Outline: User attaches an image file to an outgoing email with two recipients
+	
+		Given the user is logged into Gmail
+    And that the user selects the compose button
+    When the user adds a recipient
+    And the user adds another recipient
+    And adds a message title
+    And attaches an image file to the email
+    And the user hits the send button
+    Then the email with the image will be succesfully sent to the correct recipients
+    
+    
+    
+    
+    
+# Error Flow
+	Scenario Outline: User attaches an image file to an outgoing email with an invalid recipient email address
+	
+	  Given the user is logged into Gmail
+    And that the user selects the compose button
+    When the user adds an invalid recipient email
+    And adds a message title
+    And attaches an image file to the email
+    And the user hits the send button
+    Then the email with the image will not be correctly sent
+    
